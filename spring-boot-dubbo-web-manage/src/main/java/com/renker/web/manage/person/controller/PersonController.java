42f95a74c7d638c029cf97beb10f86d7f8c6c29c
renker <caishenchen@163.com>
2017-02-09 10:43:05 +0800

    调整

---
+++ spring-boot-dubbo-web-manage/src/main/java/com/renker/web/manage/person/controller/PersonController.java
@@ -0,0 +1,21 @@
+package com.renker.web.manage.person.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.renker.dubbo.api.person.service.IPersonService;
+
+@Controller
+@RequestMapping("manage/person")
+public class PersonController {
+	
+	@Reference(group="person")
+	public IPersonService personService;
+	
+	@RequestMapping(value="index",method={RequestMethod.GET,RequestMethod.POST})
+	public String index(){
+		return "person/index";
+	}
+}
