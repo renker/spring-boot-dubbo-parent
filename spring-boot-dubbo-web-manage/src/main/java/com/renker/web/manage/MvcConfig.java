323f2c0f4d998b231c9178c01c10be83922ee1c3
renker <caishenchen@163.com>
2017-02-10 16:06:42 +0800

    1

--- spring-boot-dubbo-web-manage/src/main/java/com/renker/web/manage/MvcConfig.java
+++ spring-boot-dubbo-web-manage/src/main/java/com/renker/web/manage/MvcConfig.java
@@ -1,23 +1,29 @@
 package com.renker.web.manage;
 
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-public class MvcConfig {
+public class MvcConfig extends WebMvcConfigurerAdapter{
 	@Value(value="${spring.mvc.view.prefix}")
 	private String spring_mvc_view_prefix;
 	
 	@Value(value="${spring.mvc.view.suffix}")
 	private String spring_mvc_view_suffix;
+	
+	@Value(value="${spring.mvc.static-path-pattern}")
+	private String pathPatterns;
+	
+	@Value(value="${spring.resources.static-locations}")
+	private String resourceLocations;
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver(){
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -25,5 +31,10 @@ public class MvcConfig {
 		viewResolver.setSuffix(spring_mvc_view_suffix);
 		viewResolver.setViewClass(JstlView.class);
 		return viewResolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(pathPatterns).addResourceLocations(resourceLocations);
 	}
 }
